2279ee2e66520cf1b88dbe5ac58989767be1f21e
troymeeker <troymeeker1@gmail.com>
2021-12-15 10:18:41 -0800

    commit tree 7be6b24a310bd926ff26a2a7db576a0709cf48c7

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(names)
+   names.map do |n|
+    "Hello, my name is #{n}."
+   end
+
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |s, room|
+        "Hello, #{s}! You'll be assigned to room #{room}!"
+    end
+  
+end
+
+def printer(speakers)
+    batch_badge_creator(speakers).each do |s|
+        puts s
+    end
+
+    assign_rooms(speakers).each do |s|
+        puts s  
+    end 
+end
